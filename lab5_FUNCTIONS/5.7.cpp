d55e049ca20728d07909e2f6e131ff895854442d
Mary <43139654+marrygran28@users.noreply.github.com>
2020-05-15 16:22:54 +0300

    Update 5.7.cpp

--- lab5_FUNCTIONS/5.7.cpp
+++ lab5_FUNCTIONS/5.7.cpp
@@ -1,52 +1,57 @@
 #include "5.7_func.h"
 
 
-//Дан массив чисел произвольной длины. Отсортировать массив заданными
-//методами :
-//сортировкой бинарными вставками и сортировкой выбором;
+//Р”Р°РЅ РјР°СЃСЃРёРІ С‡РёСЃРµР» РїСЂРѕРёР·РІРѕР»СЊРЅРѕР№ РґР»РёРЅС‹. РћС‚СЃРѕСЂС‚РёСЂРѕРІР°С‚СЊ РјР°СЃСЃРёРІ Р·Р°РґР°РЅРЅС‹РјРё
+//РјРµС‚РѕРґР°РјРё :
+//СЃРѕСЂС‚РёСЂРѕРІРєРѕР№ Р±РёРЅР°СЂРЅС‹РјРё РІСЃС‚Р°РІРєР°РјРё Рё СЃРѕСЂС‚РёСЂРѕРІРєРѕР№ РІС‹Р±РѕСЂРѕРј;
 
-//Для каждого метода сортировки определить число сравнений и перемещений(перестановок с одного места на другое) элементов в процессе выполнения программы.
-//Сравнить экспериментальные результаты с известными теоретическими оценками этих показателей для заданных методов сортировки.
-//Обеспечить перегрузку и шаблоны необходимых функций для выполнения задания с типами элементов массивов char, int, float, double.
+//Р”Р»СЏ РєР°Р¶РґРѕРіРѕ РјРµС‚РѕРґР° СЃРѕСЂС‚РёСЂРѕРІРєРё РѕРїСЂРµРґРµР»РёС‚СЊ С‡РёСЃР»Рѕ СЃСЂР°РІРЅРµРЅРёР№ Рё РїРµСЂРµРјРµС‰РµРЅРёР№(РїРµСЂРµСЃС‚Р°РЅРѕРІРѕРє СЃ РѕРґРЅРѕРіРѕ РјРµСЃС‚Р° РЅР° РґСЂСѓРіРѕРµ) СЌР»РµРјРµРЅС‚РѕРІ РІ РїСЂРѕС†РµСЃСЃРµ РІС‹РїРѕР»РЅРµРЅРёСЏ РїСЂРѕРіСЂР°РјРјС‹.
+//РЎСЂР°РІРЅРёС‚СЊ СЌРєСЃРїРµСЂРёРјРµРЅС‚Р°Р»СЊРЅС‹Рµ СЂРµР·СѓР»СЊС‚Р°С‚С‹ СЃ РёР·РІРµСЃС‚РЅС‹РјРё С‚РµРѕСЂРµС‚РёС‡РµСЃРєРёРјРё РѕС†РµРЅРєР°РјРё СЌС‚РёС… РїРѕРєР°Р·Р°С‚РµР»РµР№ РґР»СЏ Р·Р°РґР°РЅРЅС‹С… РјРµС‚РѕРґРѕРІ СЃРѕСЂС‚РёСЂРѕРІРєРё.
+//РћР±РµСЃРїРµС‡РёС‚СЊ РїРµСЂРµРіСЂСѓР·РєСѓ Рё С€Р°Р±Р»РѕРЅС‹ РЅРµРѕР±С…РѕРґРёРјС‹С… С„СѓРЅРєС†РёР№ РґР»СЏ РІС‹РїРѕР»РЅРµРЅРёСЏ Р·Р°РґР°РЅРёСЏ СЃ С‚РёРїР°РјРё СЌР»РµРјРµРЅС‚РѕРІ РјР°СЃСЃРёРІРѕРІ char, int, float, double.
 
 int main()
 {
     int size = 10;
     int* I = new int[size];
     int* I_copy = new int[size];
-    char* C = new char[size];
-    char* C_copy = new char[size];
 
-    cout << endl << "\nCHAR" << endl;
-    initArrayRandom(C, size);
-    copy(C, C_copy, size);
+    cout << endl << "\nRANDOM" << endl;
 
-    printArray(C, size);
-    cout << "\nSelection sort:\n";
-    SelectionSort(C, size);
- 
-    printArray(C, size);
-    cout << "\nBinary sort:\n";
-    BinarySort(C_copy, size);
-    printArray(C_copy, size);
-
-    cout << endl << "\nINT" << endl;
-    initArrayDescendingOrder(I, size);
+    initArrayRandom(I, size);
     copy(I, I_copy, size);
     printArray(I, size);
-
     cout << "\nSelection sort:\n";
     SelectionSort(I, size);
     printArray(I, size);
+    cout << "\nBinary sort:\n";
+    BinarySort(I_copy, size);
+    printArray(I_copy, size);
 
+    cout << endl << "\nDESCENDING" << endl;
+    initArrayDescendingOrder(I, size);
+    copy(I, I_copy, size);
+    printArray(I, size);
+    cout << "\nSelection sort:\n";
+    SelectionSort(I, size);
+    printArray(I, size);
+    cout << "\nBinary sort:\n";
+    BinarySort(I_copy, size);
+    printArray(I_copy, size);
+
+    cout << endl << "\nASCENDING" << endl;
+    initArrayAscendingOrder(I, size);
+    copy(I, I_copy, size);
+    cout << endl;
+    printArray(I, size);
+    cout << "\nSelection sort:\n";
+    SelectionSort(I, size);
+    printArray(I, size);
     cout << "\nBinary sort:\n";
     BinarySort(I_copy, size);
     printArray(I_copy, size);
 
     delete[] I;
-    delete[] C;
     delete[] I_copy;
-    delete[] C_copy;
     return 0;
 }
 
